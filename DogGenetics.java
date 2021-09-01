1a799582caa23d38152ea8a4c20d5d5cfadf66d9
danhuynh1 <danh8953@gmail.com>
2021-09-01 15:21:23 -0400

    Update DogGenetics.java
    Added More possible breeds, Made number of ancestors configurable.

--- DogGenetics.java
+++ DogGenetics.java
@@ -1,9 +1,8 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class DogGenetics {
-    static String[] dogTypes = {"St. Bernard","Chihuahua","Dramatic RedNosed Asian Pug","Common Cur","King Doberman"};
+    static String[] dogTypes = {"St. Bernard","Chihuahua","Dramatic RedNosed Asian Pug","Common Cur","King Doberman","Labrador Retriever", "Airedale Terrier", "Shiba Inu", "West Highland White Terrier", "Boykin Spaniel", "Cardigan Welsh Corgi"};
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String dogName;
@@ -11,13 +10,16 @@ public class DogGenetics {
         System.out.print("What is your dog's name:");
         dogName = sc.nextLine();
 
-        int[] dogPercents = getRandomPercentages();
+        int numberOfAncestors = getIntFromUser(sc, "How many ancestors do you want to see?");
+        int[] dogPercents = getRandomPercentages(numberOfAncestors);
+        
+
         int randomStartingPosition = new Random().nextInt(4);
         System.out.printf("Well then, I have this highly reliable report on %s's prestigious background right here.\n\n%s is: \n",dogName,dogName);
 
         //Prints dog types from a random starting position.
         for(int i =0; i<dogTypes.length;i++){
-            System.out.printf("%d%% %s\n",dogPercents[i],dogTypes[(randomStartingPosition++)%5]);
+            System.out.printf("%d%% %s\n",dogPercents[i],dogTypes[(randomStartingPosition++)%numberOfAncestors]);
         }
         sc.close();
     }
@@ -27,17 +29,36 @@ public class DogGenetics {
      * todo: remove hardcoded 5
      * @return percentageArray, an int array of size 5 containing 5 random numbers that sum up to 100
      */
-    public static int[] getRandomPercentages(){
+    public static int[] getRandomPercentages(int numOfAncestors){
         int totalLeft = 100;
         int currentPercent;
-        int percentageArray[] = {0,0,0,0,0};
+        int percentageArray[] = new int[numOfAncestors];
+
         for (int i = 0; i < percentageArray.length;i++){
-            currentPercent= new Random().nextInt(totalLeft) ;
+            currentPercent= new Random().nextInt(totalLeft+1) ;
             percentageArray[i] = currentPercent;
             totalLeft -= currentPercent;
         }
+
         //Remaining percentage is added to last instance.
-        percentageArray[4]+=totalLeft;
+        percentageArray[numOfAncestors-1]+=totalLeft;
+
         return percentageArray;
+    }
+
+    public static int getIntFromUser(Scanner sc, String prompt){
+        int value;
+        do {
+            System.out.printf("%s: ",prompt);
+            try {
+                value = Integer.parseInt(sc.nextLine());
+                break;
+ 
+    
+            } catch(NumberFormatException ex) {
+                System.out.println("Please input a correct value.");
+            }
+        }while(true);
+        return value;
     }
 }
