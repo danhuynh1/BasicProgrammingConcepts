e2044dd30d63ee9db2490bb5525a5769ee62223b
danhuynh1 <danh8953@gmail.com>
2021-09-02 10:01:28 -0400

    Update HealthyHearts.java

--- HealthyHearts.java
+++ HealthyHearts.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 public class HealthyHearts {
     public static void main(String[] args){
         Scanner inputReader = new Scanner(System.in);
-        System.out.print("What is your age? ");
-        int age = Integer.parseInt(inputReader.nextLine());
+        int age = getIntFromUser(inputReader, "What is your age? ");
         int maximum = 220-age;
         System.out.printf("Your maximum heart rate should be %d beats per minute\n",maximum);
         
@@ -13,5 +12,28 @@ public class HealthyHearts {
         System.out.printf("Your target HR Zone is %d - %d.\n",lowEnd,highEnd);
         inputReader.close();
         
+    }
+
+        /**
+     * Helper method to get an int from prompt
+     * @param sc
+     * @param prompt
+     * @return
+     */
+
+    public static int getIntFromUser(Scanner sc, String prompt){
+        int value;
+        do {
+            System.out.printf("%s: ",prompt);
+            try {
+                value = Integer.parseInt(sc.nextLine());
+                break;
+ 
+    
+            } catch(NumberFormatException ex) {
+                System.out.println("Please input a correct value.");
+            }
+        }while(true);
+        return value;
     }
 }
